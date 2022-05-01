feb1198bb3dd539c0f25aa7db64469cc2678c384
Carmela Valeria Rivero <103906161+CarmelaRivero@users.noreply.github.com>
2022-05-01 20:33:33 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from ClaseManejadorCama import ManejadorCama
+from ClaseManejadorMedicamento import ManejadorMedicamento
+
+
+if __name__ == '__main__':
+    manejadorCama = ManejadorCama()
+    manejadorMedicamento = ManejadorMedicamento()
+    manejadorCama.testCamas()
+    manejadorMedicamento.testMedicamentos()
+    print('---ALTA DE PACIENTE---')
+    print('Ingrese nombre y apellido del paciente')
+    nomApe = input('Nombre y Apellido: ')
+
+
